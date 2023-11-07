e7311107e7fbb64951bf6241f54f8bb223542661
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-07 18:38:24 +0000

    started again

---
+++ pygame/main.py
@@ -0,0 +1,18 @@
+import pygame
+import pygame.locals
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
