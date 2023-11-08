c4642ec3d682c1024e73eefadfe49dc70e7cfe3f
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-08 18:24:21 +0000

    added ufo

--- pygame/main.py
+++ pygame/main.py
@@ -7,8 +7,11 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+#title and icon
+pygame.display.set_caption("Space Invaders")
 
+
+running = True
 # game loop
 while running:
     for event in pygame.event.get():
