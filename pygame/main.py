8562049f83e5166dc2be3adfe8c55363deb785fd
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-08 18:46:26 +0000

    change

--- pygame/main.py
+++ pygame/main.py
@@ -13,15 +13,26 @@ icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
 # player
-playerImg = pygame.image.load('player.png')
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 
 running = True
 # game loop
 while running:
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # RGB
-    screen.fill((0, 0, 0))
+
+    player()
+
     pygame.display.update()
