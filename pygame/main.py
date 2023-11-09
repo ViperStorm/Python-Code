8cec8d9ec187f6910cafd2e11f858dc227a08253
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-09 17:54:25 +0000

    movement

--- pygame/main.py
+++ pygame/main.py
@@ -17,19 +17,31 @@ playerImg = pygame.image.load("player.png")
 playerX = 370
 playerY = 480
 
-def player(x,y):
+
+def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
 running = True
 # game loop
 while running:
-    #RGB
+    # RGB
     screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            print("a keystroke has been pressed")
+            if event.key == pygame.K_LEFT:
+                print("Left arrow is pressed")
+            elif event.key == pygame.K_RIGHT:
+                print("Right arrow is pressed")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Keystroke has been released")
 
     player(playerX, playerY)
 
