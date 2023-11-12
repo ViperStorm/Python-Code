a801c2d5e4c4ffe950fbcc7260360a0ff19f2db3
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-12 11:46:15 +0000

    Turtle hw

--- main.py
+++ main.py
@@ -1,63 +1,55 @@
-from calendar import day_abbr
+import turtle
+
+# wn
+wn = turtle.Screen()
+wn.bgcolor("lightblue")
+wn.title("My initials")
+wn.setup(800, 600)
+
+# t
+t = turtle.Turtle()
+t.shape("turtle")
+t.shapesize(2)
+t.pensize(5)
+t.pencolor("black")
+t.speed(5)
+t.penup()
+t.left(180)
+t.forward(50)
+t.pendown()
+t.right(45)
+t.forward(70)
+t.right(180)
+t.forward(70)
+t.left(90)
+t.forward(70)
+t.right(180)
+t.forward(70)
+t.left(45)
+t.forward(50)
+t.penup()
+t.left(90)
+
+for i in range(2):
+    if i == 0:
+        t.forward(75)  # move to the next letter
+    else:
+        t.forward(30)
+
+    t.pendown()
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(60)
+    t.penup()
+    t.right(90)
+    t.forward(100)
+    t.left(90)
 
 
-users = ["Dave", "John", "Sara"]
-
-data = ['Yohan', 42, True]
-
-emptylist = []
-
-print(users[0])
-print(users[-2])
-
-print(users.index('Sara')) #Tells were Sara is at the index
-                           #note: the index is at proper index
-                           #(it is at index 3) even though it is technically at index 2
-
-print(users[0:2])
-print(users[1:])
-print(users[-3:-1]) 
-
-print(len(data))
-
-users.append('Elsa') #*1
-print(users)
-
-users += ['Jason'] # *1
-print(users)
-
-users.extend(['Robert', 'Jimmy']) #*1
-print(users)
-
-# users.extend(data) # Does the same thing *1
-# print(users)
-
-users.insert(0, 'Bob')
-print(users)
-
-users[2:2] = ['Eddie','Alex']
-print(users)
-
-users[1:3] = ['Robert', 'JPJ']
-print(users)
-
-users.remove('Bob')
-print(users)
-
-print(users.pop())
-print(users)
-
-del users[0]
-print(users)
-
-data.clear()
-# del data #Deletes data
-# print(data) 
-print(data)
-
-users[1:2] = ['dave']
-users.sort()
-print(users)
-
-users.sort(key=str.lower)
-print(users)
+wn.mainloop()
