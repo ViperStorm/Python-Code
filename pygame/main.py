b1d8c65ac91227007ee372dda122ad96282d5f99
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-12 16:41:20 +0000

    added use of bullets for shooting

--- pygame/main.py
+++ pygame/main.py
@@ -1,4 +1,3 @@
-from turtle import back
 import pygame
 import random
 
@@ -8,7 +7,7 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-#add background
+# add background
 background = pygame.image.load("background.jpg")
 
 
@@ -16,7 +15,6 @@ background = pygame.image.load("background.jpg")
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
-
 
 
 # player
@@ -29,8 +27,15 @@ playerX_change = 0
 enemyImg = pygame.image.load("enemy.png")
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.2
+enemyX_change = 2
 enemyY_change = 40
+
+# bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = random.randint(0, 800)
+bulletY = random.randint(50, 150)
+bulletX_change = 2
+bulletY_change = 40
 
 
 def player(x, y):
@@ -46,9 +51,8 @@ running = True
 while running:
     # RGB
     screen.fill((0, 0, 0))
-    #background image
+    # background image
     screen.blit(background, (0, 0))
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -57,9 +61,9 @@ while running:
         # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -4
             elif event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 4
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -76,10 +80,10 @@ while running:
     enemyX += enemyX_change
 
     if enemyX <= 0:
-        enemyX_change = 0.2
+        enemyX_change = 2
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.2
+        enemyX_change = -2
         enemyY += enemyY_change
 
     player(playerX, playerY)
