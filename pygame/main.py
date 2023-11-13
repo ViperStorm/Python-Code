fe76592b6a894c04ace143a4feee3d2739efffda
ViperStorm <90562202+ViperStorm@users.noreply.github.com>
2023-11-13 17:33:19 +0000

    chatgpt fixed my code

--- pygame/main.py
+++ pygame/main.py
@@ -4,19 +4,16 @@ import random
 
 pygame.init()
 
-
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
 # add background
 background = pygame.image.load("background.jpg")
 
-
 # caption and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
-
 
 # player
 playerImg = pygame.image.load("player.png")
@@ -25,11 +22,19 @@ playerY = 480
 playerX_change = 0
 
 # enemy
-enemyImg = pygame.image.load("enemy.png")
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 2
-enemyY_change = 40
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load("enemy.png"))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(2)
+    enemyY_change.append(40)
 
 # bullet
 bulletImg = pygame.image.load("bullet.png")
@@ -47,8 +52,8 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 
 def fire_bullet(x, y):
@@ -88,13 +93,13 @@ while running:
             elif event.key == pygame.K_SPACE:
                 if bullet_state == "ready":
                     bulletX = playerX
-                    fire_bullet(playerX, bulletY)
+                    fire_bullet(bulletX, bulletY)
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    # Checking for bounderies of spaceship so it doesnt go out of bounds
+    # Checking for boundaries of the spaceship so it doesn't go out of bounds
     playerX += playerX_change
 
     if playerX <= 0:
@@ -103,14 +108,25 @@ while running:
         playerX = 736
 
     # enemy movement
-    enemyX += enemyX_change
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 2
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -2
+            enemyY[i] += enemyY_change[i]
 
-    if enemyX <= 0:
-        enemyX_change = 2
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -2
-        enemyY += enemyY_change
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
 
     # Bullet Movement
     if bulletY <= 0:
@@ -121,17 +137,7 @@ while running:
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
-    # Collision
-    collision = isCollision(enemyX, enemyY, bulletX, bulletY)
-    if collision:
-        bulletY = 480
-        bullet_state = "ready"
-        score += 1
-        enemyX = random.randint(0, 800)
-        enemyY = random.randint(50, 150)
-
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
     pygame.display.update()
 
 print(f"Score = {score}")
